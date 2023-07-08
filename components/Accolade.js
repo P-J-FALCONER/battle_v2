20e91aeb596ee4c022c429885dc369b2ad37e2a6
pat <pat@getfoodgenius.com>
2023-07-08 12:05:03 -0500

    done

--- components/Accolade.js
+++ components/Accolade.js
@@ -20,8 +20,119 @@ const Sponsor = () => {
                                     <div className="sponsor-item">
                                         <h1 className="sponsor__title">2019-2020</h1>
                                         <ul className='accolades_list'>
-                                        <li>Temp</li>
-                                        <li>Temp</li>
+                                        <li>A swimmer (Scott Piper) receiving the Big 10 Wayne Duke Postgraduate Scholarship, first student-athlete to receive the award in both program and school history.</li>
+                                        <li>Both teams achieving top 10 GPAs out of all the Division 1 swim & dive programs in the country</li>
+                                        <li>Women’s tied for 7th</li>
+                                        <li>Men’s had the 4th highest GPA</li>
+                                        <li>1 female swimmer and 1 female diver received 2020 Scholar Athlete Award (indicates these athletes have maintained a 4.0 GPA all semesters as a student athlete)</li>
+                                        <li>2 male swimmers received 2020 Scholar Athlete Award (indicates these athletes have maintained a 4.0 GPA all semesters as a student athlete)</li>
+                                        <li>Hundreds of hours volunteering in the East Lansing area (Spartan Buddies, St.Baldricks, Salvation Army, Toys for Tots, March is Reading Month, Chuck-a-Puck just to name a few)</li>
+                                        <li>22 female swimmers/ divers named as Academic All-Big Ten honorees</li>
+                                        <li>21 male swimmers/ divers named as Academic All-Big Ten honorees</li>
+                                        <li>The combination of swim & dive honorees makes up 43 of 97 winter sport Academic All-Big Ten honorees this past school year at MSU</li>
+                                        <li>A swimmer (Scott Piper) named to the First Academic All-America Team (2019, 2020)</li>
+                                        <li>A swimmer (Nehemiah Mork) named to the Second Academic All-America Team</li>
+                                        <li>A swimmer (Madeline Reilly) was named as a PNC Achievers Student Athlete of the month</li>
+                                        <li>30 swimmers/divers combined for both men’s and women’s teams were named as 2019-2020 Big Ten Distinguished Scholar Award recipients (indicates these athletes have maintained a 3.7 GPA or higher)</li>
+                                        <li>A swimmer received the Harold & Raynor Shnider Office of the Provost Award (awarded to student athlete who embodies characteristics prompted by SASS such as academic performance, community involvement, and leadership abilities)</li>
+                                        <li>16 swimmers/ divers combined from both women’s and men’s team received the Highest Honor Award at the academic gala (3.75 GPA or higher)</li>
+                                        <li>19 swimmers/ divers combined from both women’s and men’s team received the High Honor Award at the academic gala (3.50 - 3.749 GPA)</li>
+                                        <li>9 swimmers/ divers combined from both women’s and men’s team received the Honor Award at the academic gala (3.00 - 3.49 GPA)</li>
+                                        <li>2 male swimmers (out of 4 athletes total) received the President’s Award (consecutive 4.0 all 8 semesters of college while being a student athlete)</li>
+                                        <li>Women's swim/dive won the Athletics Directors Cup (highest GPA on campus)</li>
+                                        <li>A female diver (Erin Neely) is the current president of SAAC</li>
+                                        <li>swim/ dive currently has 6 people on SAAC (2 at large members, 4 regular committee members)</li>
+                                        <li>Swim & Dive (both men’s and women’s) has been the only team on campus, since coming back for the Fall 2020 semester that has been 100% Covid free</li>
+                                        <li>2019-2020 USA Swimming High School Academic All-Americans - Freshman Cris Gore and Evan Stanislaw (there could be others, I tried looking up all names)</li>
+                                        <li>Michigan State University – Board of Trustees Award 2020 (Scott Piper and Nehemiah Mork)</li>
+                                        <li>In recognition of attaining the highest cumulative grade-point average in the graduating class of Spring 2020</li>
+                                        <li>President’s Award 2019-2020 (Scott Piper and Nehemiah Mork).Presented to the male and female graduating senior with the highest cumulative grade point average and who has exhausted their eligibility and completed their degree in the 2019-2020 school year</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sponsor-wrap">
+                            <div className="row">
+                                <div className="col-lg-3">
+                                    <div className="logo-item logo-item1">
+
+                                    </div>
+                                </div>
+                                <div className="col-lg-9">
+                                    <div className="sponsor-item">
+                                        <h1 className="sponsor__title">2018-2019</h1>
+                                        <ul className='accolades_list'>
+                                        <li>Google Cloud CoSIDA Academic All-American First Team 2019 (Scott Piper)</li>
+                                        <li>Cloud CoSIDA Academic All-American Second Team 2019 (Courtney Aycock)</li>
+                                        <li>Google Cloud CoSIDA Academic First Team All-District 2019 (Courtney Aycock and Scott Piper)</li>
+                                        <li>Big Ten Postgraduate Scholarship Winner 2019 (Ellie Roche)</li>
+                                        <li>20 swimmers were Big Ten Distinguished Scholars</li>
+                                        <li>36 swimmers were Academic All-Big Ten Honors</li>
+                                        <li>Michigan State University – Board of Trustees Award 2019 (Courtney Aycock)</li>
+                                        <li>In recognition of attaining the highest cumulative grade-point average in the graduating class of Spring 2019</li>
+                                        <li>Athletic Director’s Award – Women’s Swim & Dive team</li>
+                                        <li>Presented to the male and female team with the highest cumulative grade point average after the fall 2018 semester</li>
+                                        <li>12 swimmer / divers received Honor Award at Academic Gala (3.00 – 3.49 GPA)</li>
+                                        <li>15 swimmer / divers received High Honor Award at Academic Gala (3.50 – 3.749 GPA)</li>
+                                        <li>14 swimmer / divers received Highest Honor Award at Academic Gala (3.75 – 4.00 GPA)</li>
+                                        <li>President’s Award 2018-2019 (Courtney Aycock)</li>
+                                        <li>Presented to the male and female graduating senior with the highest cumulative grade point average and who has exhausted their eligibility and completed their degree in the 2018-2019 school year</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sponsor-wrap">
+                            <div className="row">
+                                <div className="col-lg-3">
+                                    <div className="logo-item logo-item1">
+
+                                    </div>
+                                </div>
+                                <div className="col-lg-9">
+                                    <div className="sponsor-item">
+                                        <h1 className="sponsor__title">2017-2018</h1>
+                                        <ul className='accolades_list'>
+                                        <li>14 swimmers were Big Ten Distinguished Scholars</li>
+                                        <li>37 swimmers were Academic All-Big Ten Honors</li>
+                                        <li>18 swimmer who received Honor Award at Academic Gala (3.00 – 3.49 GPA)</li>
+                                        <li>14 swimmers received High Honor Award at Academic Gala (3.50 – 3.749 GPA)</li>
+                                        <li>8 swimmers received Highest Honor Award at Academic Gala (3.75 – 4.00 GPA)</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sponsor-wrap">
+                            <div className="row">
+                                <div className="col-lg-3">
+                                    <div className="logo-item logo-item1">
+
+                                    </div>
+                                </div>
+                                <div className="col-lg-9">
+                                    <div className="sponsor-item">
+                                        <h1 className="sponsor__title">2014-2015</h1>
+                                        <ul className='accolades_list'>
+                                        <li>David Zoltowski Rhode Scholar Finalist</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="sponsor-wrap">
+                            <div className="row">
+                                <div className="col-lg-3">
+                                    <div className="logo-item logo-item1">
+
+                                    </div>
+                                </div>
+                                <div className="col-lg-9">
+                                    <div className="sponsor-item">
+                                        <h1 className="sponsor__title">2006-2007</h1>
+                                        <ul className='accolades_list'>
+                                        <li>Matt McNichols selected as University’s representative for NCAA National Leadership Conference</li>
                                         </ul>
                                     </div>
                                 </div>
