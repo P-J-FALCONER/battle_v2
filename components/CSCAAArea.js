f4229fa84651293bf62fb497a74473fb5c33d8f9
pat <pat@getfoodgenius.com>
2023-09-09 18:09:53 -0500

    adding pdf to index page

---
+++ components/CSCAAArea.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CSCAAArea = () => {
+    return (
+        <section className="about-area text-center">
+            <div className="container">
+                <div className="row">
+                <div className="col-lg-12">
+                <h3 className="involve_title">A case for college swimming and diving</h3>
+                    </div>
+                    <div className="col-lg-12">
+                        <div className="about-large-img align-items-center">
+                            <img src="/images/MSU_CSCAA.jpeg" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CSCAAArea;
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import NavOne from "../components/NavOne";
 import SliderTwo from "../components/SliderTwo";
 import CallToActionTwo from "../components/CallToActionTwo";
 import FaqArea from "../components/FaqArea";
+import CSCAAArea from "../components/CSCAAArea";
 
 const HomePage = () => {
     return (
         <Layout pageTitle="Battle for Spartan Swim & Dive">
           <NavOne />
           <SliderTwo />
+          <CSCAAArea />
           <FaqArea />
           <CallToActionTwo />
         </Layout>
